ded008ff9f354db7acf5f2f7bcd86798248ecaab
holylovenia <holy.lovenia@gmail.com>
2018-03-01 16:14:44 +0700

    Added DetectionTaskLoader

---
+++ app/src/main/java/com/hulahoop/mentalhealth/undepress/loaders/DetectionTaskLoader.java
@@ -0,0 +1,29 @@
+package com.hulahoop.mentalhealth.undepress.loaders;
+
+import android.content.Context;
+import android.support.v4.content.AsyncTaskLoader;
+
+import com.hulahoop.mentalhealth.undepress.NetworkUtils;
+
+/**
+ * Created by agoun on 2/27/2018.
+ */
+
+public class DetectionTaskLoader extends AsyncTaskLoader<String> {
+    private String mAccessToken;
+
+    public DetectionTaskLoader(Context context, String accessToken) {
+        super(context);
+        mAccessToken = accessToken;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+    @Override
+    public String loadInBackground() {
+        return NetworkUtils.getResponse("detection", "GET", null, mAccessToken);
+    }
+}
